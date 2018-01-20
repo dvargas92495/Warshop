1a62e16b053f0d3275953dbbad6913f91119866d
unknown <dvargas92495@gmail.com>
2018-01-20 10:38:12 -0500

    menu item hover

--- Assets/Scripts/UI/MenuItemController.cs
+++ Assets/Scripts/UI/MenuItemController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 public class MenuItemController : MonoBehaviour {
 
     Action callback;
+    public MeshRenderer background;
+    public Material onHover;
+    public Material offHover;
+
+    void OnMouseEnter()
+    {
+        background.material = onHover;
+    }
+
+    void OnMouseExit()
+    {
+        background.material = offHover;
+    }
 
     void OnMouseUp()
     {
