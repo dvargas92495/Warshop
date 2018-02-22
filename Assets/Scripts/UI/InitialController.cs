8a1fe0aa32f0dcd1b37223156ba76b0fedb09590
Bill Kelleher <billykelleher24@gmail.com>
2018-02-21 22:50:27 -0500

    Updated roster selection

--- Assets/Scripts/UI/InitialController.cs
+++ Assets/Scripts/UI/InitialController.cs
@@ -4,14 +4,16 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.Rendering;
 using UnityEngine.Networking;
+using TMPro;
+using System.Linq;
+using System.Collections.Specialized;
 
 public class InitialController : MonoBehaviour {
 
     private bool isServer;
-    private float dx = 200;
-    private float dy = 50;
-    private int width = 2;
+    private byte myStarCount = 0;
     public Button loadBoardButton;
+    public Button startGameButton;
     public InputField myName;
     public InputField opponentName;
     public Button myAdd;
@@ -28,15 +30,36 @@ public class InitialController : MonoBehaviour {
     public TextAsset[] boardfiles;
     public RobotController robotBase;
     public Sprite[] robotDir;
+    public Sprite[] squadSpriteDir;
+    public GameObject robotRosterImage;
+    public GameObject maximizedRosterRobot;
+    public GameObject robotSquadImage;
+    public GameObject robotRosterPanel;
+    public GameObject robotSelectedPanel;
+    public GameObject squadPanelHolder;
+    public GameObject squadBackgroundPanel;
+    public GameObject squadPanel;
+    public GameObject maximizedRosterRobotInfoPanel;
+    public string robotSelection;
+
 
     //TEMP JUST FOR PLAYTEST: DELETE
     public Text starText;
-    private Dictionary<string, byte> starRatings = new Dictionary<string, byte>()
+    private Dictionary<string, byte[]> robotDictionary = new Dictionary<string, byte[]>()
     {
-        {"Bronze Grunt", 1 },
-        {"Silver Grunt", 2 },
-        {"Golden Grunt", 3 },
-        {"Platinum Grunt", 4 },
+        // Rating, attack, health
+        {"Bronze Grunt", new byte[] {1,3,2}},
+        {"Silver Grunt", new byte[] {2,8,3}},
+        {"Golden Grunt", new byte[] {3,5,10}},
+        {"Platinum Grunt",new byte[] {4,6,15}},
+    };
+    private Dictionary<string, string[]> abilityDictionary = new Dictionary<string, string[]>()
+    {
+        // Rating, attack, health
+        {"Bronze Grunt", new string[] {"None",""}},
+        {"Silver Grunt", new string[] {"None",""}},
+        {"Golden Grunt", new string[] {"None",""}},
+        {"Platinum Grunt",new string[] {"None",""}},
     };
 
     public void Awake()
@@ -47,6 +70,7 @@ public class InitialController : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
+        RosterController.InitializeInitial(this);
         Logger.Setup(isServer);
         App.LinkAssets(boardfiles);
         if (isServer)
@@ -103,67 +127,197 @@ public class InitialController : MonoBehaviour {
             localModeToggle.gameObject.SetActive(false);
             useServerToggle.gameObject.SetActive(false);
         }
-        int x = -1;
-        int y = 2;
-        foreach (TextAsset t in boardfiles)
-        {
-            Button thisBoard = Instantiate(loadBoardButton, transform);
-            thisBoard.GetComponentInChildren<Text>().text = t.name;
-            RectTransform rect = thisBoard.GetComponent<RectTransform>();
-            rect.anchoredPosition = new Vector2(x * dx, y * dy);
-            x++;
-            if (x >= width)
-            {
-                x = -2;
-                y--;
+
+
+        startGameButton.onClick.AddListener(() =>
+            {                           
+                if (myStarCount == 8)
+                {
+                   GameObject squadPanelRobotHolder = GameObject.Find("SquadPanelsHolder/My Squads/Squad Panel0/SquadPanelRobotHolder");
+                   string[] myRosterStrings = new string[squadPanelRobotHolder.transform.childCount];
+                   for(int i = 0; i < myRosterStrings.Length; i++)
+                   {
+                        myRosterStrings[i] = squadPanelRobotHolder.transform.GetChild(i).name.Trim();
+                   }
+
+                    GameObject oppSquadPanelRobotHolder = GameObject.Find("SquadPanelsHolder/Opponent Squads/Squad Panel0/SquadPanelRobotHolder");
+                    string[] opponentRosterStrings = new string[oppSquadPanelRobotHolder.transform.childCount];
+                    for (int i = 0; i < opponentRosterStrings.Length; i++)
+                    {
+                        opponentRosterStrings[i] = oppSquadPanelRobotHolder.transform.GetChild(i).name.Trim();
+                    }
+
+
+                    StartGame(
+                          "Battery",
+                          (myRosterStrings),
+                          (GameConstants.LOCAL_MODE && opponentRosterStrings.Length > 0 ? opponentRosterStrings : new string[0]),
+                          myName.text,
+                          (opponentName.IsActive() ? opponentName.text : "")
+                      );
+                }
             }
-            thisBoard.onClick.AddListener(() =>
-            {
-                if (byte.Parse(starText.text.Substring(0, 1)) != 8) return;
-                StartGame(
-                    t.name,
-                    (myRoster.text.Length > 0 ? myRoster.text.Substring(0, myRoster.text.Length - 1).Split('\n') : new string[0]),
-                    (opponentRoster.IsActive() && opponentRoster.text.Length > 0 ? opponentRoster.text.Substring(0, opponentRoster.text.Length - 1).Split('\n') : new string[0]),
-                    myName.text, 
-                    (opponentName.IsActive() ? opponentName.text : "")
-                );
-            });
-        }
-        myAdd.onClick.AddListener(() =>
-        {
-            string robot = mySelect.options[mySelect.value].text;
-            byte count = byte.Parse(starText.text.Substring(0,1));
-            if (starRatings[robot] + count <= 8)
-            {
-                count += starRatings[robot];
-                myRoster.text += robot + "\n";
-                starText.text = count + "/8 STARS";
-                if (count == 8) starText.text += " READY!";
-            }
-        });
-        opponentAdd.onClick.AddListener(() =>
-        {
-            string robot = opponentSelect.options[opponentSelect.value].text;
-            opponentRoster.text += robot + "\n";
-        });
-        List<Dropdown.OptionData> opts = new List<Dropdown.OptionData>();
+        );
+
+        
+
+       
         foreach (Sprite r in robotDir)
         {
-            Dropdown.OptionData opt = new Dropdown.OptionData();
-            opt.text = r.name;
-            opts.Add(opt);
+            GameObject rosterRobot = Instantiate(robotRosterImage, robotRosterPanel.transform);
+            rosterRobot.name = r.name;
+            rosterRobot.GetComponent<Image>().sprite = r;
+            rosterRobot.GetComponent<Button>().onClick.AddListener(() => RosterController.maximizeRobot(rosterRobot.name));
+
+
         }
-        mySelect.AddOptions(opts);
-        opponentSelect.AddOptions(opts);
+
+
+        squadPanelsCreate(GameConstants.LOCAL_MODE);
+
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space)) { myRoster.text = ""; starText.text = "0/8 STARS"; }
+       // if (Input.GetKeyUp(KeyCode.Space)) { myRoster.text = ""; starText.text = "0/8 STARS"; }
+    }
+
+
+   public void maximizeSelection(string selection)
+    {
+        GameObject robotSelectionPanel = robotSelectedPanel;
+        foreach (Transform child in robotSelectionPanel.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        if (selection != "no selection")
+        {
+           
+            foreach (Sprite r in robotDir)
+            {
+                if (selection == r.name)
+                {
+                    GameObject selectedRobot = Instantiate(maximizedRosterRobot, robotSelectedPanel.transform);
+                    selectedRobot.name = selection;
+                    selectedRobot.GetComponent<Image>().sprite = r;
+                    robotSelection = selectedRobot.name;
+
+                    GameObject selectedRobotInfo = Instantiate(maximizedRosterRobotInfoPanel, robotSelectedPanel.transform);
+                    TMP_Text[] fields = selectedRobotInfo.GetComponentsInChildren<TMP_Text>();
+                    fields[0].SetText(robotSelection);
+                    if (robotDictionary[robotSelection][0] == 1)
+                    {
+                        fields[1].SetText("Rating: Bronze");
+                    }
+                    if (robotDictionary[robotSelection][0] == 2)
+                    {
+                        fields[1].SetText("Rating: Silver");
+                    }
+                    if (robotDictionary[robotSelection][0] == 3)
+                    {
+                        fields[1].SetText("Rating: Gold");
+                    }
+                    if (robotDictionary[robotSelection][0] == 4)
+                    {
+                        fields[1].SetText("Rating: Platinum");
+                    }
+                    fields[2].SetText("Attack: " + robotDictionary[robotSelection][1].ToString());
+                    fields[3].SetText("Health: " + robotDictionary[robotSelection][2].ToString());
+                    fields[4].SetText("Ability: " + abilityDictionary[robotSelection][0] + abilityDictionary[robotSelection][1]);
+
+
+                }
+            }
+        }
+        else
+        {
+            robotSelection = "no selection";
+        }
+
+
+    }
+
+    public void squadPanelsCreate(bool localMode)
+    {
+        GameObject squadPanelsHolder = squadPanelHolder;
+        GameObject mySquads = Instantiate(squadBackgroundPanel, squadPanelsHolder.transform);
+        mySquads.name = "My Squads";
+        createIndividualSquads(mySquads.transform, mySquads.name);
+        if (localMode)
+        { 
+            GameObject opponentSquads = Instantiate(squadBackgroundPanel, squadPanelsHolder.transform);
+            opponentSquads.name = "Opponent Squads";
+            createIndividualSquads(opponentSquads.transform, opponentSquads.name);
+        }
+
+    }
+
+    public void createIndividualSquads(Transform squadBackground, string squadsOwner)
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            GameObject currentSquadPanel = Instantiate(squadPanel, squadBackground);
+            currentSquadPanel.name = "Squad Panel" + i.ToString();
+            Transform currentSquadButton = currentSquadPanel.transform.GetChild(0);
+            currentSquadButton.GetComponent<Button>().onClick.AddListener(() => RosterController.addToSquad(squadsOwner, currentSquadPanel.name));
+            currentSquadButton.GetComponent<Image>().sprite = squadSpriteDir[i];
+        }
+    }
+
+    public void addSelectedToSquad(string squadOwner, string squadName)
+    {
+        if (robotSelection != "no selection")
+        {
+            GameObject squadPanelRobotHolder = GameObject.Find(squadOwner + "/" + squadName + "/SquadPanelRobotHolder");
+            GameObject addedRobot = Instantiate(robotSquadImage, squadPanelRobotHolder.transform);
+            addedRobot.name = robotSelection;
+            addedRobot.GetComponent<Button>().onClick.AddListener(() => RosterController.removeFromSquad(squadOwner, squadName, addedRobot));
+            foreach (Sprite r in robotDir)
+            {
+                if (robotSelection == r.name)
+                {
+                    addedRobot.GetComponent<Image>().sprite = r;
+                }
+            }
+            if (squadOwner == "My Squads")
+            {
+                myStarCount += robotDictionary[robotSelection][0];
+                starText.text = myStarCount.ToString() + "/8";
+                if (myStarCount == 8)
+                {
+                    startGameButton.interactable = true;
+                }
+                else
+                {
+                    startGameButton.interactable = false;
+                }
+            }
+ 
+            maximizeSelection("no selection");
+        }
+    }
+
+    public void removeAddedFromSquad(string squadOwner, string squadName, GameObject robotName)
+    {
+        if (squadOwner == "My Squads")
+        {
+            myStarCount -= robotDictionary[robotName.name][0];
+            starText.text = myStarCount.ToString() + "/8";
+            if (myStarCount == 8)
+            {
+                startGameButton.interactable = true;
+            }
+            else
+            {
+                startGameButton.interactable = false;
+            }
+        }
+        GameObject.Destroy(robotName);
     }
 
     void StartGame(string b, string[] mybots, string[] opbots, string myname, string opponentname)
     {
+
         RobotController.robotBase = robotBase;
         RobotController.robotDir = robotDir;
         Interpreter.myRobotNames = mybots;
---
+++ Assets/Scripts/UI/RosterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RosterController {
+
+    internal static InitialController initialController;
+
+    public static void InitializeInitial(InitialController ic)
+    {
+        initialController = ic;
+    }
+
+    public static void maximizeRobot(string name)
+    {
+        initialController.maximizeSelection(name);
+    }
+
+    public static void addToSquad(string squadOwner, string squadName)
+    {
+        initialController.addSelectedToSquad(squadOwner, squadName);
+    }
+    public static void removeFromSquad(string squadOwner, string squadName, GameObject robotName)
+    {
+        initialController.removeAddedFromSquad(squadOwner, squadName, robotName);
+    }
+
+}
