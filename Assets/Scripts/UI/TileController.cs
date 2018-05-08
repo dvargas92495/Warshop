9a1a8365a7ba6b64c9941d4431ee5fe222b039d5
Bill Kelleher <billykelleher24@gmail.com>
2018-05-08 18:05:09 -0400

    adding core

--- Assets/Scripts/UI/TileController.cs
+++ Assets/Scripts/UI/TileController.cs
@@ -38,7 +38,7 @@ public class TileController : MonoBehaviour {
             GameObject Battery = Instantiate(battery, transform);
             Battery.transform.localRotation = Quaternion.Euler(Vector3.left * 90);
             Battery.transform.localPosition = Vector3.back * 0.5f;
-            Battery.transform.localScale += Vector3.up * ((1 / transform.localScale.z) - 1);
+           //Battery.transform.localScale += Vector3.up * ((1 / transform.localScale.z) - 1);
             return BoardController.BATTERY_TYPE;
         }
         
