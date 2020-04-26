4f1547ab90434482127286311d2ae48933e74c68
David Vargas <dvargas92495@gmail.com>
2020-04-26 18:58:22 -0400

    More typos

--- Terraform/aws.tf
+++ Terraform/aws.tf
@@ -283,7 +283,7 @@ resource "aws_api_gateway_integration" "integration" {
   for_each                = toset(local.lambdas)
 
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-  resource_id             = aws_api_gateway_resource.resource[local.lambda_paths[each.value]].id
+  resource_id             = aws_api_gateway_resource.resource[local.paths[each.value]].id
   http_method             = upper(local.methods[each.value])
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -295,7 +295,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function[each.value].function_name
+  function_name = aws_lambda_function.lambda[each.value].function_name
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
